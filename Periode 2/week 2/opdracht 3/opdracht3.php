8f0c35efe99074448f96ca0ea129e692f593bf19
Jack-van-Bommel <9019233@student.zadkine.nl>
2022-12-02 13:00:00 +0100

    opdracht 3 af

--- Periode 2/week 2/opdracht 3/opdracht3.php
+++ Periode 2/week 2/opdracht 3/opdracht3.php
@@ -4,11 +4,21 @@
 </head>
 <body>
     <?php
+    // 2 random string variabelen
     $stringvariable1 = "Dit is de eerste string variable.";
     $stringvariable2 = "Dit is de tweede string variable.";
     echo $stringvariable1;
     echo "<br>";
     echo $stringvariable2;
+
+    echo "<br>";
+
+    // 2 random nummer variabelen
+    $intvariable1 = 12;
+    $intvariable2 = 15;
+    echo $intvariable1;
+    echo "<br>";
+    echo $intvariable2;
     ?>
 </body>
 </html>
