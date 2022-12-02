efb1768be60d1fc9136fec58c2bb2dfeb567f0c4
Jack-van-Bommel <9019233@student.zadkine.nl>
2022-12-02 12:57:48 +0100

    start opdracht 3

---
+++ Periode 2/week 2/opdracht 3/opdracht3.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Opdracht 3</title>
+</head>
+<body>
+    <?php
+    $stringvariable1 = "Dit is de eerste string variable.";
+    $stringvariable2 = "Dit is de tweede string variable.";
+    echo $stringvariable1;
+    echo "<br>";
+    echo $stringvariable2;
+    ?>
+</body>
+</html>
