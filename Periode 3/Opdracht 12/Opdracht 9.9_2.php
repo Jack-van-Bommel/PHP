341dcc4305824825e394e04c89307b6ef0c44395
Jack-van-Bommel <9019233@student.zadkine.nl>
2023-02-03 11:23:31 +0100

    wip opdracht 12 h9.9 1, 2 en 3

--- Periode 3/Opdracht 12/Opdracht 9.9_2.php
+++ Periode 3/Opdracht 12/Opdracht 9.9_2.php
@@ -4,12 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Opdracht 12</title>
+    <title>Opdracht 12: 9.9 opdracht 2</title>
 </head>
 <body>
     <?php
 
-//Kan nie verder tot vorige opgaven zijn gemaakt
 
     ?>
 </body>
