93051a32dbf9d7fceabac4f06aad210f44309416
Jack-van-Bommel <9019233@student.zadkine.nl>
2023-02-09 11:08:22 +0100

    Teams ontwerpen php opdrachten afgemaakt

--- Periode 3/ontwerp lessen/7 feb les/action_page_Jack.php
+++ Periode 3/ontwerp lessen/7 feb les/action_page_Jack.php
@@ -6,6 +6,7 @@
 
 <?php
 
+//inlog gedeelte
 $username_length = strlen($_POST["username"]);
 
 echo "Your username is: ", $_POST["username"], "<br>";
@@ -18,25 +19,28 @@ switch($username_length){
         echo "ERROR: Username must have a minimum of 5 symbols", "<br>";
 }
 echo "Your password is: ", $_POST["password"], "<br>";
-
 echo "<br><br>";
 
 
-
-$a[0] = "jan";
-$a[1] = "rob";
-$a[2] = "piet";
-$b = array(10, 11, 12, 34, 100, 1000);
+//array 
+$a = ["jan", "rob", "piet"];
 
 echo "<table border=1 width=200>";
-echo "<tr><td>";
-
-foreach($b as $value){
-    echo $value. "<br>";
-};
-
-echo "</td></tr>";
+foreach ($a as &$data){
+    echo "<tr><td>";
+    echo $data;
+    echo "</td></tr>";
+}
 echo "</table>";
+echo "<br><br>";
+
+
+//inlog as table
+echo "<table border=1 width=200>";
+echo "<tr><td>Username</td><td>" . $_POST["username"] . "</td></tr>";
+echo "<tr><td>Password</td><td>" . $_POST["password"] . "</td></tr>";
+echo "</table><br><br>";
+// express geen foreach gebruikt aangezien we maar 1 set gegevens hebbens
 
 ?>
 
