e39372e2d58ebaee94b55afd2f52db6214bdb2c8
Jack <9019233@student.zadkine.nl>
2023-04-03 12:10:28 +0200

    opdracht 10.5 verder gegaan

--- Periode 3/Verdieping/10.5.php
+++ Periode 3/Verdieping/10.5.php
@@ -12,30 +12,75 @@
 
 function connectdb(){
     try {
-        $db = new PDO("mysql:host=localhost;dbname=poll", "root", "");
-        $queryread = $db->prepare("SELECT optie, stemmen FROM opties");
-        $queryread->execute();      
-        return $queryread;
+        $db = new PDO("mysql:host=localhost;dbname=polls", "root", "");
+        $queryread = $db->prepare("SELECT * FROM optie");
+        $queryread->execute();    
+        $result = $queryread->fetchALL(PDO::FETCH_ASSOC);
+        return $result;
     }
     catch (PDOException $e) {
         die("Error: " . $e->getmessage());
     }
 }
 
-
-$queryread = connectdb();
-
-
-echo "<table border=1px>";
-echo "<tr><th>Stelling van de maand: 'PHP is de leukste programmeertaal!'</th></tr>";
-echo "<tr><td>Aantal uitgebrachte stemmen: $totaalstemmen</td></tr>";
-foreach ($queryread as $data) {
-    echo "<tr>";
-    echo "<td>", $data["optie"], "</td>";
-    echo "<td>", $data["stemmen"], "</td>";
-    echo "</tr>";
+function calculatetotal($result) {
+    $optie1 = $result[0]["stemmen"];
+    $optie2 = $result[1]["stemmen"];
+    $optie3 = $result[2]["stemmen"];
+    $optie4 = $result[3]["stemmen"];
+    $total = $optie1 + $optie2 + $optie3 + $optie4;
+    return $total;
 }
-echo "</table>";
+
+function calculatepercentage($result, $total) {
+    $optie1 = $result[0]["stemmen"];
+    $optie2 = $result[1]["stemmen"];
+    $optie3 = $result[2]["stemmen"];
+    $optie4 = $result[3]["stemmen"];
+    
+    // Formula = klein getal / total x 100 = percentage
+    $calc1 = $optie1 / $total * 100;
+    $percent1 = round($calc1, 2) . "%";
+    $calc2 = $optie2 / $total * 100;
+    $percent2 = round($calc2, 2) . "%";
+    $calc3 = $optie3 / $total * 100;
+    $percent3 = round($calc3, 2) . "%";
+    $calc4 = $optie4 / $total * 100;
+    $percent4 = round($calc4, 2) . "%";
+
+    $percent = array($percent1, $percent2, $percent3, $percent4);
+    return $percent;
+}
+
+function printtable($result, $total, $percent) {
+    $table = "<table border=1px>";
+
+    $table .= "<tr><th>" . "Stelling van de maand: 'PHP is de leukste programmertaal'" . "</th></tr>";
+    $table .= "<tr><td>" . "Aantal uitgebrachte stemmen: " . $total . "</td></tr>";
+    
+
+    foreach ($result as $data) {
+        $table .= "<tr>";
+        $table .= "<td>" . $data["optie"] . "</td>";
+        $table .= "<td>" . $data["stemmen"] . "</td>";
+    }
+    foreach ($result as $cell) {
+        for($i = $result;$i++) {
+
+        }
+        $table .= "</tr>";
+    }
+
+    echo $table;
+}
+
+
+$result = connectdb();
+$total = calculatetotal($result);
+$percent = calculatepercentage($result, $total);
+printtable($result, $total, $percent);
+
+
 
 ?>
 
